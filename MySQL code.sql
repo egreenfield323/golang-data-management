db72f4954271fd5f660bc4f7514a63d702c3f95e
Evan Greenfield <evangreenfield@live.com>
2025-01-30 23:34:53 -0500

    add mysql database code

---
+++ MySQL code.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE data_management;
+
+USE data_management;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
